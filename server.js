8c9fdc5afa0743b50a447320fba67a54941bd12a
sountharya <sountharyaks@gmail.com>
2025-01-07 21:26:09 +0530

    .env change day1

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const toyRoutes = require('./routes/toyRoutes');
 const path = require('path');
@@ -7,21 +6,25 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+mongoose.connect('MONGODB_URL/toyshop').then(() => {
+    console.log('MongoDB atlas connected');
+}).catch(err => {
+    console.error('MongoDB atlas connection error:', err);
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://127.0.0.1:27017/toyshop').then(() => {
-    console.log('MongoDB connected');
-}).catch(err => {
-    console.error('MongoDB connection error:', err);
-});
-
 app.get('/', (req, res) => {
-    res.render('frontPage'); 
+    res.render('frontPage');
 });
 
-app.use('/toys', toyRoutes); 
+app.use('/toys', toyRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
